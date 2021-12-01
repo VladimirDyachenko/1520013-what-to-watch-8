72e3bf3562068d70fc94e116565b173528707872
VladimirDyachenko <d.vovan3@gmail.com>
2021-12-01 14:09:07 +0300

    Исправил логотип в футторе

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../utils/const';
 
-function Logo(): JSX.Element {
+type LogoLinkProps = {
+  additionalClassName?: string;
+}
+
+function Logo(props: LogoLinkProps): JSX.Element {
   return (
     <div className="logo">
-      <Link to={AppRoute.Main} className="logo__link">
+      <Link to={AppRoute.Main} className={`logo__link ${props.additionalClassName}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
